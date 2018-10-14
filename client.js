20ba585ae00860c6189c2bbb635c420d4ee75edc
Wenxin Zhou <wenxinmmb@gmail.com>
2018-10-14 13:39:58 -0700

    protocol part

--- client.js
+++ client.js
@@ -1,19 +1,78 @@
 const net = require('net');
+const crypto = require('crypto');
+const aesWrapper = require('./component/aes-wrapper');
+
 let client;
+let shared_secret = 'scambooter';
+let client_nonce;
+let nonce_of_server; 
+let client_dh;
+let client_dh_key;
+let client_dh_secret;
 
 function start(host, port) {
     if (client === undefined) {
         client = new net.Socket();
 
         client.connect(port, host, () => {
+            // TODO: get shared secret from UI client
             console.log("Client is connected to " + host + ":" + port);
-
-            // TODO: Authentication on connection
-            // send('Welcome Server!');
         });
 
         client.on('data', function (data) {
-            //TODO: decrypt data
+            if(data.byteLength < 3){
+                console.log('Data received not valid!');
+                return;
+            }     
+            //console.log('Data received', data);
+            const code = data.toString().slice(0,3);
+            switch (code) {
+                case '301':
+                //Receive Auth1 - server send DH public key and nonce Rb
+                //Store nonce Rb and DH g and p
+                console.log('Receive Auth - 1');
+                nonce_of_server = data.slice(3,15);
+                client_nonce = crypto.randomBytes(12);
+                let dh_prime = data.slice(15,79); //change this later hardcoded this length for dh prime 512b = 64B
+                let dh_generator = data.slice(79,80);
+                client_dh = crypto.createDiffieHellman(dh_prime,dh_generator);
+                client_dh_key = client_dh.generateKeys();
+
+                //Send Auth2 - client send Ra and E(Rb nonce, g^b mod p)
+                let auth2 = aesWrapper.createAesMessage(get32B(shared_secret), Buffer.concat([nonce_of_server,client_dh_key]));
+                send(Buffer.concat([Buffer.from('101'), client_nonce, Buffer.from(auth2)]));
+                console.log('Send Auth - 2');
+                break;
+
+                case '302':
+                //Auth3 - server send E(Ra nonce, g^a mod p)
+                //Extract g^a mod p, calculate session key = g^ab mod p
+                let aes_msg2 = data.slice(3,data.byteLength);
+                let aes_raw2 = Buffer.from(aesWrapper.decrypt(get32B(shared_secret),aes_msg2.toString()),'hex');
+                let nonce_of_client_from_server = aes_raw2.slice(0,12);
+                let dh_key_of_server = aes_raw2.slice(12,aes_raw2.byteLength);
+                if(nonce_of_client_from_server.equals(client_nonce)){
+                    console.log('Client authentication pass');
+                }
+                client_dh_secret = client_dh.computeSecret(dh_key_of_server);
+                console.log('Receive Auth -3');
+
+                //Start sending msg with session key client_dh_secret(//want to earse client_dh and client_dh_key?)
+                let msg_from_UI ='We use AES encryption for this assignment.';
+                send_encry(msg_from_UI);
+                break;
+
+                case '303':
+                //Decrypted Msg - server send AES encrypted message with session key = g^ab mod p
+                decrypt(data);
+                //TODO: display this raw and decrypted message in UI client    
+                break;
+
+                default:
+                console.log('Code not valid', code);
+            }
+            
+
         });
 
         client.on('close', function () {
@@ -26,12 +85,33 @@ function start(host, port) {
 
 function send(data) {
     if (client) {
-        //TODO: encrypt data
-
         client.write(data);
-        console.log(data);
+        //console.log(data);
     } else {
         console.error("The client connection must be started first.");
+    }
+}
+
+//Encrypted Msg - using AES shared session key
+function send_encry(msg){
+    if(client_dh_secret){
+        let encry_msg = aesWrapper.createAesMessage(client_dh_secret.slice(0,32),Buffer.from(msg));
+        send(Buffer.from('102'+ encry_msg));
+        //TODO: get message to send from UI client, display encrypted message
+        console.log('Send message: ', msg);
+    }else{
+        console.error("Mutual Authentication failed.");
+    }
+}
+
+function decrypt(enc_msg){
+    if(client_dh_secret){
+        let encryted_aes_msg = enc_msg.slice(3, enc_msg.byteLength);
+        let decrypted_aes_msg = Buffer.from(aesWrapper.decrypt(client_dh_secret.slice(0,32),encryted_aes_msg.toString()),'hex');
+        //TODO: display encrypted message in UI
+        console.log("Receive message:",decrypted_aes_msg.toString());
+    }else{
+        console.error("Mutual Authentication failed.");
     }
 }
 
@@ -45,6 +125,16 @@ function stop() {
     }
 }
 
+function get32B(sdata){
+    let buffer = Buffer.from(sdata);
+    if(buffer.byteLength > 32)
+        return buffer.slice(0,32);
+    else
+        return Buffer.from(sdata.padEnd(32, '0'));
+}
+
+
+start("127.0.0.1", "8081");
 //Exports
 module.exports.start = start;
 module.exports.send = send;
--- server.js
+++ server.js
@@ -1,18 +1,79 @@
 const net = require('net');
+const crypto = require('crypto');
+const aesWrapper = require('./component/aes-wrapper');
 
 //Single instance of server
 let server, socket;
+let shared_secret = 'scambooter';
+let server_nonce ;
+let server_dh;
+let server_dh_key;
+let server_dh_secret;
+let nonce_of_client;
+
 
 function start(host, port) {
     if (server === undefined) {
         server = net.createServer(listener => {
             socket = listener;
-
-            // TODO: Authentication on connection
             console.log("Server is connected to client.");
 
+            // TODO: get shared secret from UI client
+            
+            // generate DH public key g and p
+            server_nonce = crypto.randomBytes(12);
+            server_dh = crypto.createDiffieHellman(512); //can use (2048), this number is in bits
+            server_dh_key = server_dh.generateKeys();
+
+            // Send Auth1 - server send DH public key and nonce Rb
+            var buffer = Buffer.concat([Buffer.from('301'), server_nonce, server_dh.getPrime(), server_dh.getGenerator()]);
+            send(buffer);
+            console.log('Send Auth-1');
+                //console.log("Send Auth - 1: ", buffer);
+                //console.log("prime: ",server_dh.getPrime());
+                //console.log("generator: ",server_dh.getGenerator());
+
+
             listener.on('data', data => {
-                //TODO: decrypt data
+                if(data.byteLength < 3){
+                    console.log('Data received not valid!');
+                    return;
+                }  
+                //console.log('Data received', data);
+                const code = data.toString().slice(0,3);
+                switch (code) {
+                    case '101':
+                        //Receive Auth2 
+                        //save nonce_client, compare nonce_server, calculate g^(ab) mode p
+                        nonce_of_client = data.slice(3,15);
+
+                        let aes_msg = data.slice(15,data.byteLength);
+                        let aes_raw = Buffer.from(aesWrapper.decrypt(get32B(shared_secret),aes_msg.toString()),'hex');
+                        let nonce_of_server_from_client = aes_raw.slice(0,12);
+                        if(nonce_of_server_from_client.equals(server_nonce)){
+                            console.log('Server authentication pass');
+                        }
+                        let dh_key_of_client = aes_raw.slice(12, aes_raw.byteLength);
+                        server_dh_secret = server_dh.computeSecret(dh_key_of_client);
+                        console.log('Receive Auth-2');
+
+                        //Send Auth3 - server send E(Ra nonce, g^a mod p)
+                        let auth3 = aesWrapper.createAesMessage(get32B(shared_secret), Buffer.concat([nonce_of_client,server_dh_key]));
+                        send(Buffer.concat([Buffer.from('302'), Buffer.from(auth3)]));
+                        console.log('Send Auth-3');
+                        break;
+
+                    case '102':
+                        //decrypt data
+                        decrypt(data);
+                        send_encry("Got your message");
+                        break;
+                        
+                        
+                    default:
+                        console.log('Code not valid', code);
+                }
+                
             });
 
         });
@@ -41,12 +102,34 @@ function start(host, port) {
 
 function send(data) {
     if (socket) {
-        //TODO: encrypt data
-
         socket.write(data);
-        console.log(data);
+        //console.log("sent data", data);
     } else {
         console.error("The server must be started first.");
+    }
+}
+
+//Encrypt Msg - using AES shared session key
+function send_encry(msg){
+    if(server_dh_secret){
+        let encry_msg = aesWrapper.createAesMessage(server_dh_secret.slice(0,32),Buffer.from(msg));
+        send(Buffer.from('303'+ encry_msg));
+        console.log('Send message: ', msg);
+    }else{
+        console.error("Mutual Authentication failed.");
+    }
+}
+
+//Decrypt Msg - using AES shared session key
+function decrypt(encry_msg){
+    if(server_dh_secret){
+        let encryted_aes_msg = encry_msg.slice(3, encry_msg.byteLength);
+        let decrypted_aes_msg = Buffer.from(aesWrapper.decrypt(server_dh_secret.slice(0,32),encryted_aes_msg.toString()),'hex');
+        console.log("Receive message:",decrypted_aes_msg.toString());
+        //TODO: display this raw and decrypted message in UI client 
+    }
+    else{
+        console.log("Mutual Authentication failed.");
     }
 }
 
@@ -60,6 +143,16 @@ function stop() {
         console.error("The server is not running. ");
     }
 }
+
+function get32B(sdata){
+    let buffer = Buffer.from(sdata);
+    if(buffer.byteLength > 32)
+        return buffer.slice(0, 32);
+    else
+        return Buffer.from(sdata.padEnd(32, '0'));
+}
+
+start("127.0.0.1", "8081");
 
 //Exports
 module.exports.start = start;
